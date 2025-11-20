27a175a10d589b9318f9ae35cbdf7bdc3eefff22
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2025-11-21 00:23:24 +0530

    Initial local commit

---
+++ src/test/java/com/popcorn/service/UserServiceUnitTest.java
@@ -0,0 +1,18 @@
+package com.popcorn.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class UserServiceUnitTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void testGetCity() {
+        assertEquals("Singapore", userService.getCity());
+    }
+}
